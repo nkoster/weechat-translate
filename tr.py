8293bad0ae8e362b6905089b5cacd98d1f023b15
pX <48695402+px-havok@users.noreply.github.com>
2019-04-01 13:25:27 -0700

    fixed py3 input bug, added py3-ok tag
    If using py3 and input includes accented characters, the urllib request would fail.  Added a py version check and using urllib.parse.quote to quote the text being sent in the url string.
    I've tested this script in weechat 2.2 and 2.4 using both py2 and py3 modules, it works pretty great.  :)

--- tr.py
+++ tr.py
@@ -1,9 +1,13 @@
 ## Language translation, by Niels, niels@w3b.net, license is GPL3.
-# v0.0.3: Updated to support Python3 -@pX <px@havok.org>
+# v0.0.3: Updated to support Python3 -@pX <px@havok.org> 03.30.2019
+# v0.0.4: Py3 translate would fail if input included accented
+#       : characters -@pX <px@havok.org> 04.01.2019
+#       : py3-ok
 
 import weechat
 import sys
 
+# try for py2 urllib2, else load py3 urllib
 try:
     import urllib2
     ulib = urllib2
@@ -24,13 +28,21 @@ def tr_cb(data, buffer, args):
     if len(a) < 2:
         weechat.prnt(weechat.current_buffer(), '/tr:\tUsage /tr lang[,lang] text')
         return weechat.WEECHAT_RC_OK
+
     o = 'nl'
     l = a[0]
     ol = l.split(',')
     tl = ol[0]
+
     if len(ol) > 1:
         o = ol[1]
-    t = ' '.join(a[1:])
+
+    # fix py3 translation issue, else py2
+    if sys.version_info >= (3,3):
+        t = urllib.parse.quote(' '.join(a[1:]))
+    else:
+        t = ' '.join(a[1:])
+
     url = 'https://translate.googleapis.com/translate_a/single' + \
         '?client=gtx&sl=' + o + '&tl=' + tl + '&dt=t&q=' + t
     url = url.replace(' ', '%20')
@@ -43,6 +55,7 @@ def tr_cb(data, buffer, args):
 
     html = response.read()
 
+    # support for py3, else py2
     if sys.version_info >= (3, 3):
         tr = html.decode().split('"')[1]
     else:
